b1892e2017f8fcb306a8cff741c403697403be5d
chenjingyu <chenjingyu@videopls.com>
2020-12-08 09:43:11 +0800

    init project

---
+++ src/main/java/com/huan/jvm/BuyController.java
@@ -0,0 +1,20 @@
+package com.huan.jvm;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BuyController {
+    @RequestMapping("/buy")
+    public String rushToBuy() {
+        List<Byte[]> list = new ArrayList<>();
+
+        Byte[] data = new Byte[1024*1024];
+        list.add(data);
+
+        return "Success";
+    }
+}
---
+++ src/test/java/com/huan/jvm/JvmOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huan.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
